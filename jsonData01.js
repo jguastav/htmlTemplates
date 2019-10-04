54d754903a3c9518f1c4b9e3f7240e88941e9bd7
jguastav <jguastav@gmail.com>
2019-10-03 21:15:46 -0300

    project creation - html static Templates reading json data

---
+++ jsonData01.js
@@ -0,0 +1,22 @@
+var myJson = { 
+				name: "Joseph Albertwine",
+				channel: "Yo no se tampoco",
+				youtubeUrl: "https://www.youtube.com/channel/UCvUVsODRvjdr-zDaLGWEnLg",
+				sites: [ {
+						name: "of woodworking",
+						url:"https://ofwoodworking.com/"
+					},{
+						name: "of whale",
+						url:"https://ofwhale.com/"
+					},{
+						name: "bronze4you.com",
+						url:"https://bronze4you.com/"
+					},{
+						name: "techstartingpoint.com",
+						url:"https://www.techstartingpoint.com/"
+					}
+				],
+				colorValue:'red',
+				isClassFoo: true,
+				isClassBar: false
+			}
